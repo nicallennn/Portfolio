d5cd66bd812f594c56cd5ee58b40854ace567570
Nick Allen <nick@scapplications.co.uk>
2019-01-04 21:51:49 +0000

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="custom-navbar">
+    <!-- flat removes drop shadow and makes opaque -->
+    <v-toolbar clipped-left="false" height="100" flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="grey--text text--darken-4">
+        <span class="text-uppercase font-weight-bold mr-2">Nick Allen</span>
+        <span class="font-weight-light hidden-sm-and-down">- Software Portfolio</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <div class="social-buttons hidden-xs-and-down">
+        <v-btn small fab class="grey white--text">
+          <v-icon>blur_on</v-icon>
+        </v-btn>
+        <v-btn small fab class="grey darken-3 white--text">
+          <v-icon>music_note</v-icon>
+        </v-btn>
+        <v-btn small fab class="grey white--text">
+          <v-icon>filter_vintage</v-icon>
+        </v-btn>
+        <v-btn small fab class="grey darken-3 white--text">
+          <v-icon>email</v-icon>
+        </v-btn>
+      </div>
+    </v-toolbar>
+
+    <!-- navigation draw -->
+    <v-navigation-drawer v-model="drawer" app class="grey darken-3">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true
+    };
+  }
+};
+</script>
+
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>Home Page</h1>
 
-    <v-btn class="teal darken-1 white--text">Click Me</v-btn>
+    <!-- <v-btn class="teal darken-1 white--text">Click Me</v-btn>
     <v-btn depressed class="blue darken-3 white--text">Click Me</v-btn>
 
     <v-btn class="green darken-3 white--text">
@@ -12,7 +12,7 @@
 
     <v-btn fab class="purple darken-3 white--text">
       <v-icon>email</v-icon>
-    </v-btn>
+    </v-btn>-->
   </div>
 </template>
 
