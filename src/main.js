ae926cdb1a628b1dd40341d9e402aded76ce78ca
Nick Allen <nick@scapplications.co.uk>
2019-01-05 15:48:51 +0000

    fixed scroll issue when changing route

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// make window scroll to top when route is changed
+router.beforeEach(function(to, from, next) {
+  window.scrollTo(0, 0);
+  next();
+});
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
